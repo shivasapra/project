f7d469d95981282802b2d7cf78fb3ecdf641f5a6
shiva <ashishsapra7@gmail.com>
2017-07-18 21:43:08 -0700

    templates file added

--- instagram_clone/signup/apps.py
+++ instagram_clone/signup/apps.py
@@ -4,5 +4,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class DemoappConfig(AppConfig):
-    name = 'demoapp'
+class SignupConfig(AppConfig):
+    name = 'signup'
