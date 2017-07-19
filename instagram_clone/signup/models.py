0653a8e2f69a0a49ecaa58c938adcb1ad827112a
shiva <ashishsapra7@gmail.com>
2017-07-18 21:56:04 -0700

    model created

--- instagram_clone/signup/models.py
+++ instagram_clone/signup/models.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class UserModel(models.model):
+    Email = models.EmailField()
+    name = models.CharField(max_length=120)
+    username = models.CharField(max_length=120)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
