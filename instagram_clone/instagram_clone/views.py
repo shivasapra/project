395f0c806003ea22086186fe8004f38ab8ac09ad
shiva <ashishsapra7@gmail.com>
2017-07-18 23:39:35 -0700

    html and views file modified

--- instagram_clone/instagram_clone/views.py
+++ instagram_clone/instagram_clone/views.py
@@ -2,9 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here
 
+
 def signup_view(request):
     #business logic.
-    return render(request,'signup.html')
+    today = datetime.now
+    return render(request,'signup.html',{'today':today})
